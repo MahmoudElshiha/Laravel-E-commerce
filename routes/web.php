f0b63bf904f3ef36dfadf96aeaf7c1ef7f7df7d7
Mahmoud_Elshiha <mahmoudelshiha@gmail.com>
2025-04-26 16:34:08 +0300

    Refactor:  using resource controller

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
-Route::get('/products/{product}/edit', [ProductController::class, 'edit']);
-Route::patch('/products/{product}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::resource('products', ProductController::class);
 
 Route::view('/contact', 'contact');
