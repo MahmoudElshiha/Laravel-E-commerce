e1e7c013fdc8d132af9c8e3e91605f910fea274c
Mahmoud_Elshiha <mahmoudelshiha@gmail.com>
2025-04-26 16:16:45 +0300

    Refactor: implement ProductController with CRUD operations and validation

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('products.index', [
+            'products' => Product::with('vendor')->latest()->paginate(4)
+        ]);
+    }
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'name' => 'required|min:3|max:255',
+            'price' => 'required|numeric',
+        ]);
+
+        $images = [
+            "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg",
+            "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-02.jpg",
+            "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg",
+            "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg",
+        ];
+        $image = fake()->randomElement($images);
+
+        Product::create([
+            'name' => request('name'),
+            'price' => request('price'),
+            'image' => $image,
+            'vendor_id' => 1
+        ]);
+
+        return redirect('/products');
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.show', [
+            'product' => $product
+        ]);
+    }
+    public function edit(Product $product)
+    {
+        return view('products.edit', [
+            'product' => $product
+        ]);
+    }
+    public function update(Product $product)
+    {
+        request()->validate([
+            'name' => 'required|min:3|max:255',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update([
+            'name' => request('name'),
+            'price' => request('price'),
+        ]);
+
+        return redirect('/products/' . $product->id);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect('/products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
+use \App\Http\Controllers\ProductController;
 
 
 
@@ -12,76 +13,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/products', function () {
-    return view('products.index', [
-        'products' => Product::with('vendor')->latest()->paginate(4)
-    ]);
-});
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::get('/products/{product}/edit', [ProductController::class, 'edit']);
+Route::patch('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
-
-Route::get('/products/create', function () {
-    return view('products.create');
-});
-
-
-Route::post('/products', function () {
-    request()->validate([
-        'name' => 'required|min:3|max:255',
-        'price' => 'required|numeric',
-    ]);
-
-    $images = [
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg",
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-02.jpg",
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg",
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    ];
-    $image = fake()->randomElement($images);
-
-    Product::create([
-        'name' => request('name'),
-        'price' => request('price'),
-        'image' => $image,
-        'vendor_id' => 1
-    ]);
-
-    return redirect('/products');
-});
-Route::get('/products/{product}', function (Product $product) {
-    return view('products.show', [
-        'product' => $product
-    ]);
-});
-
-Route::get('/products/{product}/edit', function (Product $product) {
-    return view('products.edit', [
-        'product' => $product
-    ]);
-});
-
-Route::patch('/products/{product}', function (Product $product) {
-    request()->validate([
-        'name' => 'required|min:3|max:255',
-        'price' => 'required|numeric',
-    ]);
-
-    $product->update([
-        'name' => request('name'),
-        'price' => request('price'),
-    ]);
-
-    return redirect('/products/' . $product->id);
-});
-
-Route::delete('/products/{id}', function () {
-
-    $product = Product::find(request('id'));
-    $product->delete();
-    return redirect('/products');
-});
-
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
