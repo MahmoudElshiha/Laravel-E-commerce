1eecc3839008c3a378a4a4dcaea3b4245be8238f
Mahmoud_Elshiha <mahmoudelshiha@gmail.com>
2025-04-26 16:33:43 +0300

    Refactor: enhance edit product form layout and structure

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -48,7 +48,7 @@
         </div>
 
         <div class="mt-6 flex items-center justify-between gap-x-6">
-            <button type="button"
+            <button form="delete-product-form" type="submit"
                 class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
                 Delete </button>
             <div class="flex items-center gap-x-6">
@@ -59,7 +59,11 @@
             </div>
         </div>
     </form>
+    <form method="POST" action="/products/{{ $product->id }}" class="hidden" id="delete-product-form">
+        @csrf
+        @method('DELETE')
 
+    </form>
 
 
 </x-layout>
